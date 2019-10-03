0e9648bb8a6f0bc8ddcf6918a525564a741cf2ad
Lance Jernigan <Lance@LanceJernigan.com>
2019-10-03 15:19:08 -0400

    Prevent activation if ACF isn't active (#14)
    * Add activation handler
    
    * Add activation handler
    
    * Add acf is_active method
    
    * Bump version number

--- poppy.php
+++ poppy.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Poppy
  * Description: A WordPress popup plugin
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Pyxl Inc.
  * Author URI: https://pyxl.com
  * License: GPLv3+
@@ -14,6 +14,8 @@ defined('WPINC') || die;
 
 define(__NAMESPACE__ . '\PATH', plugin_dir_path(__FILE__));
 define(__NAMESPACE__ . '\URI', plugin_dir_url(__FILE__));
+
+register_activation_hook(__FILE__, ['Poppy\Controllers\Register', 'handler']);
 
 require_once 'lib/autoload.php';
 
