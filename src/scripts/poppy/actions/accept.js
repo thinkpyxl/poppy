58f0cbcb9c004cc70d2b81c0b5f20ff12ba5da43
Lance Jernigan <lance@lancejernigan.com>
2019-10-03 11:35:14 -0400

    Remove popup on action

--- src/scripts/poppy/actions/accept.js
+++ src/scripts/poppy/actions/accept.js
@@ -4,6 +4,13 @@ const accept = (popup, slug) => ({ currentTarget }) => {
   popup.classList.add('inactive');
 
   storage.setItem(`poppy_${slug}_response`, true);
+
+  window.setTimeout(
+    300,
+    () => {
+      popup.remove();
+    }
+  );
 }
 
 export default accept;
--- src/scripts/poppy/actions/decline.js
+++ src/scripts/poppy/actions/decline.js
@@ -4,6 +4,13 @@ const dismiss = (popup, slug) => ({ currentTarget }) => {
   popup.classList.add('inactive');
   
   storage.setItem(`poppy_${slug}_response`, false);
+
+  window.setTimeout(
+    300,
+    () => {
+      popup.remove();
+    }
+  );
 }
 
 export default dismiss;
